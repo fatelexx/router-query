c25ad745794b954614cd371efdeabe3efd6a9e37
fatelexx <fatelexx@users.noreply.github.com>
2025-04-27 07:47:42 +0000

    deploy: aa34150796b6c60f4b5daf5482d199d760e7d929

---
+++ assets/posts._postId-yzQFIYGh.js
@@ -0,0 +1,1 @@
+import{R as e,a as n,j as s}from"./index-CYScIXnh.js";import{u as a}from"./useSuspenseQuery-DlNnceet.js";const m=function(){const o=e.useParams().postId,{data:t}=a(n(o));return s.jsxs("div",{className:"space-y-2",children:[s.jsx("h4",{className:"text-xl font-bold underline",children:t.title}),s.jsx("div",{className:"text-sm",children:t.body})]})};export{m as component};
